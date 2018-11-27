54556d0e0779185aa89fb002900c9dd337e6ac83
Jimmy Wen <jimmy.wen@eatadakimasu.com>
2018-11-26 21:45:20 -0500

    Should have conditional probability distribution calculated now. Have not tested yet

--- mp440.py
+++ mp440.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #Global variables
 computedStatistics = null
 
@@ -65,30 +67,58 @@ def compute_statistics(data, label, width, height, feature_extractor, percentage
 
     #Get percentage of data
     partialDataSize = int(len(data)*percentage)
-    partialData = data[0:modifiedDataSize]
+    partialData = data[0:partialDataSize]
 
-    #Calculate prior distribution
+    #Calculate prior distribution and store indices of each occurrence of each number in indexLists
     numberOccurrence = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     priorDistribution = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    indexLists = [[], [], [], [], [], [], [], [], [], []]
     dataSize = len(data)
 
+    int index = 0
     for number in label:
         numberOccurrence[number] += 1.0
+        indexLists[number].append(index)
+        index += 1
 
-    int index = 0
+    #Reset index counter
+    index = 0
     for occurrenceCount in numberOccurrence:
         priorDistribution[index] = numberOccurrence / dataSize
+        index += 1
 
     #Calculate conditional distribution
-    #Possibly need to calculate this in for loop below for each individual image?
-        
+    conditionalProbabilitiesList = [[], [], [], [], [], [], [], [], [], []]
+    cplIndex = 0
+    #Go through numbers 0 to 9, get and store number of times number occurs in data set
+    for number in indexLists:
+        numberInstanceCount = len(number)
+        trueCount = np.zeros(len(data[number[0]]),len(data[number[0]]))
 
-    for image in partialData:
-        extractedFeatures = feature_extractor(image, width, height)
+        #Go through each instance of a number (e.g. all 1's)
+        for instanceIndex in number:
+            pixelInstanceCount = 0
+            #Get image object
+            numberImage = data[instanceIndex]
+            extractedFeatures = feature_extractor(numberImage, width, height)
 
-        
+            #Go through each pixel in the image and increment trueCount if it is a feature
+            for row in range(0, len(extractedFeatures)):
+                for column in range(0, len(extractedFeatures[row])):
+                    if(extractedFeatures[row][column]):
+                        trueCount[row][column] += 1
 
-    
+        #Calculate conditional probabilities of each pixel for this number
+        conditionalProbabilities = np.zeros(len(trueCount), len(trueCount))
+        for row in range(0, len(conditionalProbabilities)):
+            for column in range(0, len(conditionalProbabilities[row])):
+                conditionalProbabilities[row][column] = trueCount[row][column] / numberInstanceCount
+        #Now have 2D array for conditional probabilities of each pixel for a number
+        conditionalProbabilitiesList[cplIndex] = conditionalProbabilities
+
+    #Should now have list of 2D arrays containing conditional probabilities for each pixel for each number in conditionalProbabilitiesList
+
+
 
     # Your code ends here #
     #_raise_not_defined()
